5d8333141f5832642bfc29d4e42771474520251e
Anas Roud <roud2@hotmail.com>
2026-09-05 12:42:25 -0500

    update reg

--- src/main/java/edu/mum/cs/cs425/final_exam_software_engineering/service/impl/RegistrationServiceImpl.java
+++ src/main/java/edu/mum/cs/cs425/final_exam_software_engineering/service/impl/RegistrationServiceImpl.java
@@ -6,9 +6,11 @@ import edu.mum.cs.cs425.final_exam_software_engineering.repository.RegistrationR
 import edu.mum.cs.cs425.final_exam_software_engineering.repository.WorkShopRepository;
 import edu.mum.cs.cs425.final_exam_software_engineering.service.RegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -53,7 +55,8 @@ public class RegistrationServiceImpl implements RegistrationService {
         // get the capacity of the work shop and check if its above 0
         if (workShopRepository.findById(workShop).isPresent()) {
             WorkShop workshop = workShopRepository.findById(workShop).get();
-            if (workshop.getCapacity() > 0) {
+            if (workshop.getCapacity() > 0 && workshop.getRegistration_deadline().isAfter(LocalDate.now())
+            ) {
                return registrationRepository.save(registration);
             } else {
                 return registration;
